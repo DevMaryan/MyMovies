38ce459f2a193a79bd4740a791db6f6b6a776209
Maryan Shapkaroski <maryan.shapkaroski@gmail.com>
2021-03-10 20:08:12 +0100

    Detail View

---
+++ MyMovies/Controllers/Info.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMovies.Controllers
+{
+    public class Info : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
