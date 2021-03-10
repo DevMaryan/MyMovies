b08966744faf5af413cf7a5047043531c8d69d36
Maryan Shapkaroski <maryan.shapkaroski@gmail.com>
2021-03-10 22:08:48 +0100

    implemented N-Tire architecture

---
+++ MyMovies.Services/MoviesService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyMovies.Models;
+using MyMovies.Repositories;
+
+
+namespace MyMovies.Services
+{
+    public class MoviesService
+    {
+        public MoviesRepository _movieRepository { get; set; }
+       
+        public MoviesService()
+        {
+            _movieRepository = new MoviesRepository();
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return _movieRepository.GetAllMovies();
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            return _movieRepository.GetMovieById(id);
+        }
+
+        public void DeleteMovieById(int id)
+        {
+            _movieRepository.DeleteMovie(id);
+        }
+    }
+}
