895b0a2e0ef2cc42f1f8f3f5e8c8e67ff7ae0b9d
Maryan Shapkaroski <maryan.shapkaroski@gmail.com>
2021-03-13 02:53:56 +0100

    Model DataAnnotations added

--- MyMovies.Models/Movie.cs
+++ MyMovies.Models/Movie.cs
@@ -1,18 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyMovies.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength:50,MinimumLength = 3)]
         public string Title { get; set; }
 
+        [Required]
         public string ImageUrl { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 20, MinimumLength = 4)]
         public string Genre { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
     }
 }
